02b35c19de018cb1489ecca9a806d6b10162359b
Alex-Carlo <48025049+Alex-Carlo@users.noreply.github.com>
2019-06-05 23:56:06 -0500

    Add files via upload

---
+++ procesos.php
@@ -0,0 +1,23 @@
+<?php 
+// Guardar los datos recibidos en variables: 
+$nombre = $_POST['nombre']; 
+$email = $_POST['email']; 
+$telefono = $_POST['telefono']; 
+$mensaje = $_POST['mensaje']; 
+// Definir el correo de destino: 
+$dest = "tucorreo@gmail.com";  
+// Estas son cabeceras que se usan para evitar el chance que el correo llegue a Spam y sea visualizado como HTML: 
+$headers = "From: $nombre <$email>\r\n";   
+$headers .= "X-Mailer: PHP5\n"; 
+$headers .= 'MIME-Version: 1.0' . "\n"; 
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+// Aqui definimos el asunto y armamos el cuerpo del mensaje mediante concatenacion de variables (.=)
+$asunto = "Contacto"; 
+$cuerpo = "Nombre: ".$nombre."<br>"; 
+$cuerpo .= "Email: ".$email."<br>"; 
+$cuerpo .= "Telefono: ".$telefono."<br>"; 
+$cuerpo .= "Mensaje: ".$mensaje;  // anidamos el mensaje final
+// Esta es una pequena validación, que solo envie el correo si todas las variables tiene algo de contenido: 
+if($nombre != '' && $email != '' && $telefono != '' && $mensaje != ''){   
+    mail($dest,$asunto,$cuerpo,$headers); //ENVIAR! 
+} ?>
